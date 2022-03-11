fae1e88406df3b8da5b77a7d0d2819f34aafb33b
omerokumus <omerokumus3@gmail.com>
2022-03-11 14:54:20 +0300

    finishing the app

--- app/src/main/java/com/example/mycalculator_java/MainActivity.java
+++ app/src/main/java/com/example/mycalculator_java/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private TextView inputTextView;
+    private int operatorIndex = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +23,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onDigit(View view) {
+        handleDivByZero();
         inputTextView.append(((Button) view).getText());
     }
 
-    public void onOperator(View view) {
-        if (inputTextView.getText().toString().isEmpty() && ((Button) view).getText().equals("-"))
-            inputTextView.append(((Button) view).getText());
+    private void handleDivByZero() {
+        if (inputTextView.getText().toString().equals("DivByZero"))
+            inputTextView.setText("");
+    }
 
-        else if (!inputContainsOperator() && isLastEntryDigit())
+    public void onOperator(View view) {
+        handleDivByZero();
+        if (inputTextView.getText().toString().isEmpty() && ((Button) view).getText().equals("-")) {
             inputTextView.append(((Button) view).getText());
+            operatorIndex = inputTextView.getText().length() - 1;
+        } else if (!inputContainsOperator() && isLastEntryDigit()) {
+            inputTextView.append(((Button) view).getText());
+            operatorIndex = inputTextView.getText().length() - 1;
+        }
 
 
     }
@@ -38,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
 
     private boolean isLastEntryDigit() {
         String input = inputTextView.getText().toString();
-        return Character.isDigit(input.charAt(input.length()-1));
+        return Character.isDigit(input.charAt(input.length() - 1));
 
     }
 
@@ -58,13 +68,35 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onEqual(View view) {
+        String input = inputTextView.getText().toString();
+        int firstNumber = Integer.parseInt(input.substring(0, operatorIndex));
+        int secondNumber = Integer.parseInt(input.substring(operatorIndex+1));
+        String operator = getOperator();
+        switch (operator) {
+            case "+": inputTextView.setText(String.valueOf(firstNumber + secondNumber)); break;
+            case "-": inputTextView.setText(String.valueOf(firstNumber - secondNumber)); break;
+            case "*": inputTextView.setText(String.valueOf(firstNumber * secondNumber)); break;
+            case "/":
+                if (secondNumber != 0)
+                    inputTextView.setText(String.valueOf(firstNumber / secondNumber));
+                else
+                    inputTextView.setText("DivByZero");
+                break;
+        }
 
+    }
+
+    private String getOperator() {
+        return inputTextView.getText().charAt(operatorIndex) + "";
     }
 
     public void onDot(View view) {
-        inputTextView.append(((Button) view).getText());
+        handleDivByZero();
+        if (!inputTextView.getText().toString().contains("."))
+            inputTextView.append(((Button) view).getText());
 
     }
+
 
     public void onClear(View view) {
         inputTextView.setText("");
